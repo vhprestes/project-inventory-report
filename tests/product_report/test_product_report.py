af5086e24799d6e98151c2e70a812f2dc82dbc2c
vhprestes <vhprestes@gmail.com>
2023-02-01 17:30:12 -0300

    feat: test_relatorio_produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        "1",
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1",
+    )
+
+    assert ("1" in product.__repr__()) is True
+    assert ("Nicotine Polacrilex" in product.__repr__()) is True
+    assert ("Target Corporation" in product.__repr__()) is True
+    assert ("2021-02-18" in product.__repr__()) is True
+    assert ("2023-09-17" in product.__repr__()) is True
+    assert ("instrucao 1" in product.__repr__()) is True
