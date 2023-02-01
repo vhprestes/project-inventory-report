31243d1d014db3bb490451a983a1d96f6cee01e1
vhprestes <vhprestes@gmail.com>
2023-02-01 16:48:19 -0300

    feat: open_xml method

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xmltodict
 
 
 class Inventory:
@@ -11,6 +12,8 @@ class Inventory:
             return Inventory.open_csv(path, type)
         if "json" in path:
             return Inventory.open_json(path, type)
+        if "xml" in path:
+            return Inventory.open_xml(path, type)
 
     @classmethod
     def open_csv(cls, path, type):
@@ -27,6 +30,17 @@ class Inventory:
     def open_json(cls, path, type):
         with open(path) as file:
             data = json.load(file)
+            if type == "simples":
+                return SimpleReport.generate(data)
+            if type == "completo":
+                return CompleteReport.generate(data)
+            else:
+                raise ValueError("Tipo inválido")
+
+    @classmethod
+    def open_xml(cls, path, type):
+        with open(path) as file:
+            data = xmltodict.parse(file.read())['dataset']['record']
             if type == "simples":
                 return SimpleReport.generate(data)
             if type == "completo":
