9fc2496d32a471fe59e2da567962dd774c4b5efc
vhprestes <vhprestes@gmail.com>
2023-02-01 16:29:48 -0300

    feat: open_json method

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,7 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
@@ -8,14 +9,27 @@ class Inventory:
     def import_data(cls, path, type):
         if "csv" in path:
             return Inventory.open_csv(path, type)
+        if "json" in path:
+            return Inventory.open_json(path, type)
 
     @classmethod
     def open_csv(cls, path, type):
         with open(path) as file:
-            reader = csv.DictReader(file, delimiter=",", quotechar='"')
+            data = csv.DictReader(file, delimiter=",", quotechar='"')
             if type == "simples":
-                return SimpleReport.generate(list(reader))
+                return SimpleReport.generate(list(data))
             if type == "completo":
-                return CompleteReport.generate(list(reader))
+                return CompleteReport.generate(list(data))
+            else:
+                raise ValueError("Tipo inválido")
+
+    @classmethod
+    def open_json(cls, path, type):
+        with open(path) as file:
+            data = json.load(file)
+            if type == "simples":
+                return SimpleReport.generate(data)
+            if type == "completo":
+                return CompleteReport.generate(data)
             else:
                 raise ValueError("Tipo inválido")
